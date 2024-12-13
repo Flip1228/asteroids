f7be0789aed0c44ae143724f59c2ecad7b9b8864
Flip1228 <mboro6208@my.sullivan.edu>
2024-12-13 13:35:33 -0600

    C: added movement

--- player.py
+++ player.py
@@ -30,6 +30,11 @@ class Player(CircleShape):
             self.rotate(-dt)
         elif keys[pygame.K_d]:
             self.rotate(dt)
+        elif keys[pygame.K_w]:
+            self.move(dt)
+        elif keys[pygame.K_s]:
+            self.move(-dt)
             
     def move(self, dt):
-        pass
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
